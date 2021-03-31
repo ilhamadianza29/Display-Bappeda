81d27be1b9dd99df7bff5da674e806d3a85d739f
rizkidwimartanto <rizkidwimartanto@gmail.com>
2021-03-31 22:13:55 +0700

    mengubah tampilan display

--- Membuat Display Agenda/js/watch.js
+++ Membuat Display Agenda/js/watch.js
@@ -4,7 +4,7 @@
     var secs = ('0' + now.getSeconds()).slice(-2);
     var mins = ('0' + now.getMinutes()).slice(-2);
     var hr = now.getHours();
-    var Time = hr + ":" + mins + ":" + secs + " wib";
+    var Time = hr + ":" + mins + ":" + secs + " WIB";
     document.getElementById("watch").innerHTML = Time;
     requestAnimationFrame(clock);
     }
